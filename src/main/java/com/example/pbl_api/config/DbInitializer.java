f2523516a2ae7b4037e8382a12a661bc043b9933
khai <pvkk224@gmail.com>
2022-10-04 16:03:24 +0700

    4/10

--- src/main/java/com/example/pbl_api/config/DbInitializer.java
+++ src/main/java/com/example/pbl_api/config/DbInitializer.java
@@ -47,44 +47,44 @@ public class DbInitializer implements ApplicationRunner {
         }
 
 
-        if(userRepository.findAll().spliterator().getExactSizeIfKnown()==0){
-            userRepository.save(
-                    new User(
-                            "PVK",
-                            null,
-                            "DN",
-                            "123456789",
-                            true,
-                            new UserAccount("admin@gmail.com",
-                                    encoder.encode("123456"),
-                            new ArrayList<>(){
-                                {
-                                    add(new Role(1));
-                                    add(new Role(2));
-                                }
-                            }
-                            )
-                    )
-            );
-
-            userRepository.save(
-                    new User(
-                            "PVK2",
-                            null,
-                            "DN",
-                            "123456789",
-                            true,
-                            new UserAccount("member@gmail.com",
-                                    "$2a$10$.amUH9PJ311zU8tHHdqYoOFp9dZm0suVxxIiGVwSi.ky9OeVvRpdK",
-                                    new ArrayList<>(){
-                                        {
-                                            add(new Role(1));
-                                        }
-                                    }
-                            )
-                    )
-            );
-        }
+//        if(userRepository.findAll().spliterator().getExactSizeIfKnown()==0){
+//            userRepository.save(
+//                    new User(
+//                            "PVK",
+//                            null,
+//                            "DN",
+//                            "123456789",
+//                            true,
+//                            new UserAccount("admin@gmail.com",
+//                                    encoder.encode("123456"),
+//                            new ArrayList<>(){
+//                                {
+//                                    add(new Role(1));
+//                                    add(new Role(2));
+//                                }
+//                            }
+//                            )
+//                    )
+//            );
+//
+//            userRepository.save(
+//                    new User(
+//                            "PVK2",
+//                            null,
+//                            "DN",
+//                            "123456789",
+//                            true,
+//                            new UserAccount("member@gmail.com",
+//                                    "$2a$10$.amUH9PJ311zU8tHHdqYoOFp9dZm0suVxxIiGVwSi.ky9OeVvRpdK",
+//                                    new ArrayList<>(){
+//                                        {
+//                                            add(new Role(1));
+//                                        }
+//                                    }
+//                            )
+//                    )
+//            );
+//        }
 
         if(categoryRepository.findAll().spliterator().getExactSizeIfKnown()==0){
             categoryRepository.save(
