30f798defcb56de445399122b0cf6b6c43e37a66
khai <pvkk224@gmail.com>
2022-10-15 18:00:55 +0700

    15/10

--- src/main/java/com/example/pbl_api/config/WebSecurityConfig.java
+++ src/main/java/com/example/pbl_api/config/WebSecurityConfig.java
@@ -21,7 +21,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -79,13 +82,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	protected void configure(HttpSecurity http) throws Exception {
 
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-		corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000/"));
-		corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
-		corsConfiguration.setAllowCredentials(true);
-
-		http.csrf().ignoringAntMatchers("/**");
+//		CorsConfiguration corsConfiguration = new CorsConfiguration();
+//		corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+//		corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000/"));
+//		corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+//		corsConfiguration.setAllowCredentials(true);
+//
+//		http.csrf().ignoringAntMatchers("/**");
+		http.cors().and().csrf().disable();
 
 		http.authorizeRequests()
 
@@ -108,13 +112,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-		http.cors().configurationSource(request -> corsConfiguration);
+//		http.cors().configurationSource(request -> corsConfiguration);
 
 
 //		http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
 //				.authenticated().and().csrf().disable().cors().configurationSource(request -> corsConfiguration);//		corsConfiguration.setExposedHeaders(List.of("Authorization"));
 
 		// You can customize the following part based on your project, it's only a sample
+	}
+
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		final CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://backendpbl6.herokuapp.com"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+		config.setAllowCredentials(true);
+		config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return source;
 	}
 
 }
